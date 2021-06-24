04975e18054f4ad8b49ce4b65941476b16bd4699
claire ogesen <claire.ogesen.co@gmail.com>
2021-06-24 15:22:21 -0700

    off line PWA working

--- client/src/components/CartItem/index.js
+++ client/src/components/CartItem/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useStoreContext } from '../../utils/GlobalState';
 import { REMOVE_FROM_CART, UPDATE_CART_QUANTITY } from '../../utils/actions';
+import { idbPromise } from "../../utils/helpers"
 
 
 const CartItem = ({ item }) => {
@@ -11,6 +12,7 @@ const CartItem = ({ item }) => {
       type: REMOVE_FROM_CART,
       _id: item._id
     });
+    idbPromise('cart', 'delete', { ...item });
   };
 
   // manually edit the quantity of shopping cart items by typing directly in the <input> elements
@@ -22,6 +24,7 @@ const CartItem = ({ item }) => {
         type: REMOVE_FROM_CART,
         _id: item._id
       });
+      idbPromise('cart', 'delete', { ...item });
       // or just update the quanity 
     } else {
       dispatch({
@@ -29,6 +32,7 @@ const CartItem = ({ item }) => {
         _id: item._id,
         purchaseQuantity: parseInt(value)
       });
+      idbPromise('cart', 'put', { ...item, purchaseQuantity: parseInt(value) });
     }
   };
 
