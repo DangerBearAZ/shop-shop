6f5ab193f4af1bdd89893fe0f4fa492fa8648e76
claire ogesen <claire.ogesen.co@gmail.com>
2021-06-24 11:22:32 -0700

    shopping cart wroking

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -2,12 +2,13 @@ import React from "react";
 /// import React, { useState } from "react"; instructions never said to remove but i did see above made erros go buh bye
 import ProductList from "../components/ProductList";
 import CategoryMenu from "../components/CategoryMenu";
-
+import Cart from '../components/Cart';
 const Home = () => {
   return (
     <div className="container">
       <CategoryMenu />
       <ProductList />
+      <Cart />
     </div>
   );
 };
